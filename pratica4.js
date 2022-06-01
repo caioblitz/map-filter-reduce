c45eb124430fa32a9b9a1e0c6ea5e756345c11b8
caiob <caioblitz@gmail.com>
2022-06-01 14:10:17 -0300

    soma de todos os numeros de um array

---
+++ pratica4.js
@@ -0,0 +1,12 @@
+//some todos os numeros de um array
+function somaNumeros(arr){
+    return arr.reduce(function(prev,current){
+        console.log ({prev});
+        console.log ({current});
+        return prev + current;
+    });
+}
+
+const arr = [1,2];
+
+console.log (somaNumeros(arr));
